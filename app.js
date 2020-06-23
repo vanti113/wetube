7c274611e749272515e03e1f377434a115dc97a3
vanti113 <56195006+vanti113@users.noreply.github.com>
2020-06-24 00:41:59 +0900

    2020.6.24

--- app.js
+++ app.js
@@ -5,18 +5,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
 const app = express();
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter); // "/"루트에서 접속이 들어왔을시 옆의 라우터가 제공하는 url에 접속이 들어오면 라우터를 실행하는 것.
 app.use(routes.users, userRouter);
