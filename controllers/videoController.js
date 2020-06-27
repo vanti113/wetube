17a06835c39ba52654337548b05f72748f06030c
vanti113 <56195006+vanti113@users.noreply.github.com>
2020-06-28 02:34:54 +0900

    add

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,5 @@
-import { videos } from "../db.js";
-import routes from "../routes.js";
+import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) =>
   res.render("home", { pageTitle: "Home", videos });
